e93a418b61e2a7f10dc0890ca2a71637abc836e2
Nelson Trujillo <ntrujillo@localhost.localdomain>
2020-06-01 21:15:29 -0500

    fix port

--- services/email.service.js
+++ services/email.service.js
@@ -21,7 +21,7 @@ function sendEmail(from, to, subject, html) {
         "host": process.env.EMAIL_HOST,
         "port": process.env.EMAIL_PORT,
         "auth": {
-            "user": process.env.PORT.EMAIL_USER,
+            "user": process.env.EMAIL_USER,
             "pass": process.env.EMAIL_PASSWORD
         }
     });
