cbf50426d2b647e80227078a406e8e4da11412f8
Nelson Trujillo <ntrujillo@localhost.localdomain>
2020-06-01 22:24:08 -0500

    add bot

--- server.js
+++ server.js
@@ -7,19 +7,29 @@ const http = require("http");
 const https = require("https");
 const dotenv = require('dotenv');
 
+const FBBotFramework = require('fb -bot-framework');
+
+
 const app = express();
-
-
 
 //initializations
 log4js.configure('./config/log4js.json');
 dotenv.config();
+let bot = new FBBotFramework({
+    page_token: process.env.PAGE_ACCESS_TOKEN,
+    verify_token: process.env.VERIFY_TOKEN
+});
+// Setup listener for incoming messages
+bot.on('message', (userId, message) => {
+    bot.sendTextMessage(userId, 'Echo Message:' + message);
+});
 //seteo en middleware manejo de json
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'webapp')));
 
 // public routes
-app.use('/webhook', require('./controllers/facebook'));
+//app.use('/webhook', require('./controllers/facebook'));
+app.use('/webhook', bot.middleware());
 app.use('/shopper', require('./controllers/shopper'));
 app.use('/product', require('./controllers/product'));
 app.use('/blog', require('./controllers/blog'));
