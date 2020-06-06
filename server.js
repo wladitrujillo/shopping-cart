7c06538c71da658a9a44e09bba2a101ff830466f
Nelson Trujillo <ntrujillo@localhost.localdomain>
2020-06-06 17:52:34 -0500

    fix mongoose conection

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -8,13 +10,22 @@ const https = require("https");
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+
+const os = require('os');
+const ifaces = os.networkInterfaces();
+
 const app = express();
 
 //initializations
 log4js.configure('./config/log4js.json');
 dotenv.config();
 //Conexión a la base mongodb    
-mongoose.connect(process.env.MONGODB_URL, { })
+mongoose.connect(process.env.MONGODB_URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
     .catch(err => logger.error(err));
 
 
@@ -31,4 +42,30 @@ app.use('/blog', require('./controllers/blog'));
 
 console.log(`Loading PORT ${process.env.PORT} environment ${process.env.NODE_ENV}`);
 const port = process.env.PORT;
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
+app.listen(port, () => {
+
+    console.log(`App listening at http://localhost:${port}`)
+
+    for (let ifname of Object.keys(ifaces)) {
+        var alias = 0;
+        ifaces[ifname].forEach(function (iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                return;
+            }
+
+            if (alias >= 1) {
+                // this single interface has multiple ipv4 addresses
+
+                console.log(`App listening at http://${iface.address}:${port}`)
+            } else {
+                // this interface has only one ipv4 adress
+                console.log(`App listening at http://${iface.address}:${port}`)
+
+            }
+            ++alias;
+        });
+
+    }
+
+})
